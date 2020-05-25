184ec7be6548b8ec91fb06bef3a3f5f2ce8367d2
panwarv127@gmail.com <panwarv127@gmail.com>
2020-05-25 19:49:59 +0530

    Maven project 1

---
+++ HelloWorld1/src/main/java/HelloWorld.java
@@ -0,0 +1,24 @@
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("It is a hello world project...");
+		User user=new User();
+		
+		
+		
+		
+		
+		/* mvn -N io.takari:maven:wrapper 
+		 * This wrapper is Used to generate a wrapper file which consist the mvn version.
+		 * By default it take your class path maven version.
+		 * But we can change that by using this command.
+		 * mvn -N io.takari:maven:wrapper  -Dmaven=3.3.2
+		 * mvnw --version : TO know the maven wrapper version.
+		 * 
+		 */
+	}
+
+}
